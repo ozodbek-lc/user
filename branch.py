570f77691e4b748b7d4bd1b36774271075019bab
ozodbek1001 <sherbekovozodbek747@gmail.com>
2024-12-16 19:34:28 +0500

    bajarildi

--- branch.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self,name,phone):
-        self.name=name
-        self.phone=phone
-    def malumot(self):
-        return f"ismi : {self.name}\ntelefon raqamami : {self.phone}"
-
-student1=Student("ali","+998906881019")
-print(student1.malumot())
